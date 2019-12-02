3bfa07aaef8cd8d3adee9c077ea1d9084f6fd7dc
Zach Mertes <zmertes@purdue.edu>
2019-12-02 01:09:01 -0500

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,88 @@
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::env;
 
-/// Input: mass
-/// Returns: fuel, counting the fuel required for the fuel itself
-fn fuel(mass: isize) -> isize {
-    let f = (mass/3)-2;
-    if f > 0 {
-        f+fuel(f)
-    } else {
-        0
+#[derive(Debug, Clone)]
+struct Intcode {
+    /// List of instructions/memory
+    list: Vec<usize>,
+    /// Current position of the program
+    /// (misusing the term "program counter")
+    pc: usize
+}
+
+impl Intcode {
+    pub fn new(program: Vec<usize>) -> Self {
+        Intcode {
+            list: program,
+            pc: 0
+        }
+    }
+    pub fn from_text(input: String) -> Self {
+        let program = input.split(',').map(|s| s.parse().unwrap()).collect();
+        Intcode {
+            list: program,
+            pc: 0
+        }
+    }
+    pub fn state(&self) -> String {
+        self.list.iter().map(|s| s.to_string()).collect::<Vec<String>>().join(",")
+    }
+    pub fn is_halted(&self) -> bool {
+        self.list[self.pc*4] == 99
+    }
+    pub fn iterate(&mut self) -> bool {
+        match self.list[self.pc] {
+            99 => { return false; },
+            1 => {
+                let dest = self.list[self.pc+3];
+                self.list[dest] = self.list[self.list[self.pc+1]] + self.list[self.list[self.pc+2]];
+                self.pc += 4;
+            },
+            2 => {
+                let dest = self.list[self.pc+3];
+                self.list[dest] = self.list[self.list[self.pc+1]] * self.list[self.list[self.pc+2]];
+                self.pc += 4;
+            },
+            _ => panic!("invalid opcode! {}", self.list[self.pc])
+        }
+        true
     }
 }
 
-fn main() -> io::Result<()> {
-    let file = File::open("input.txt")?;
-    let reader = BufReader::new(file);
-    let mut totfuel = 0;
-    for line in reader.lines() {
-        let mass = line.unwrap().parse().unwrap();
-        totfuel += fuel(mass);
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("args: <program> [goal value]");
+    } else if args.len() == 2 {
+        let mut program = Intcode::from_text(args[1].to_string());
+        println!("{}", program.state());
+        while program.iterate() {
+            println!("{}", program.state());
+        }
+    } else if args.len() == 3 {
+        let goal = args[2].parse::<usize>().unwrap();
+        let program = Intcode::from_text(args[1].to_string());
+        let mut noun = 0;
+        let mut verb = 0;
+        let mut result = 0;
+        loop {
+            let mut program = program.clone();
+            program.list[1] = noun;
+            program.list[2] = verb;
+            while program.iterate() {}
+            result = program.list[0];
+            if result != goal {
+                noun+=1;
+                if noun == 100 && verb == 99 {
+                    println!("Failed to find anything");
+                    return;
+                }
+                if noun == 100 { noun = 0; verb += 1; }
+            } else {
+                break;
+            }
+        }
+        println!("{}", (noun*100)+verb);
     }
-    println!("fuel: {}", totfuel);
-    Ok(())
 }
 
 #[cfg(test)]
